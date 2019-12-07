87958c281aa0040d745335136cd6fe857dcec38b
h82w8-dev <56717469+h82w8-dev@users.noreply.github.com>
2019-12-07 15:24:38 +0300

    Create instagram.js

---
+++ commands/info/instagram.js
@@ -0,0 +1,44 @@
+const RichEmbed = require("discord.js");
+const stripIndets = require("common-tags");
+
+module.exports = {
+    name:"instagram",
+    aliases: ["insta"],
+    category: "info",
+    description: "Find out some instagram statistic",
+    usage: "<name>",
+    
+    run: async (client, message, args) => {
+    const name = args.join(" ");
+
+        if (!name) {
+            return message.reply("Maybe it's useful to search...")
+                .then(m => m.delete(5000));
+        }
+
+        const url = `https://instagram.com/${name}/?_a=1`;
+        const res = await fetch(url).then(url => url.json());
+
+        if (!res.graphql.user.username) {
+            return message.reply("no user find...")
+                .then(m => m.delete(5000));
+        }
+
+        const account = res.graphql.user;
+
+        const embed = new RichEmbed()
+            .setColor("RANDOM")
+            .setTitle(account.full_name)
+            .setURL(account.external_url_linkshimed)
+            .setThumbnail(account.profile_pic_url_hd)
+            .addField("Profile Information", stripIndets` **- Username:** ${account.username}
+            **- Fullname:** ${account.full_name}
+            **- Biography:** ${account.biography.lenght == 0 ? "none" : account.biography}
+            **- Posts** ${account.edge_owner_to_timeline_media}
+            **- Followers:** ${account.edge_followed_by.cont}
+            **- Following:** ${account.edge_follow.cont}
+            **- Privete account:** ${account.is_private ? "Yes 🔒" : "Nope 🔓"}`);
+
+        message.channel.send(embed);
+    }
+}
