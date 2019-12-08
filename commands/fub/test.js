030b74e28c2155ba7e9dc41990cee4d90457daf1
h82w8-dev <56717469+h82w8-dev@users.noreply.github.com>
2019-12-08 15:30:38 +0300

    Create test.js

---
+++ commands/fub/test.js
@@ -0,0 +1,23 @@
+const { RichEmbed } = require("discord.js");
+const { stripIndets } = require("common-tags");
+const fetch = require("node-fetch");
+
+module.exports = {
+    name: "test",
+    aliases: ["t"],
+    category: "fun",
+    description: "affix mb?",
+    run: async (client, message, args) => {
+        const name = args.join(" ");
+
+        if(!name) {
+            return message.reply("Maybe it's userful!")
+                .then(m => m.delete(3000));
+        }
+
+        const url = `https://instagram.com/${name}/?__a=1`;
+        const res = await fetch(url).then(url => url.json());
+
+        console.log(res);
+    }
+}
