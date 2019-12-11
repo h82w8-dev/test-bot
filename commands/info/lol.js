02805f18bee1dab75b0f06f4d71da3acbda431e6
h82w8-dev <56717469+h82w8-dev@users.noreply.github.com>
2019-12-11 17:50:35 +0300

    Update lol.js

--- commands/info/lol.js
+++ commands/info/lol.js
@@ -17,32 +17,9 @@ module.exports = {
                 .then(m => m.delete(5000));
         }
 
-        const url = `https://instagram.com/${name}/?__a=1`;
+        const url = `https://wowaffixes.info`;
+        console.log(res);
         
-        let res; 
-
-        try {
-            res = await fetch(url).then(url => url.json());
-        } catch (e) {
-            return message.reply("I couldn't find that account... :(")
-                .then(m => m.delete(5000));
-        }
-
-        const account = res.graphql.user;
-
-        const embed = new RichEmbed()
-            .setColor("RANDOM")
-            .setTitle(account.full_name)
-            .setURL(`https://instagram.com/${name}`)
-            .setThumbnail(account.profile_pic_url_hd)
-            .addField("Profile information", stripIndents`**- Username:** ${account.username}
-            **- Full name:** ${account.full_name}
-            **- Biography:** ${account.biography.length == 0 ? "none" : account.biography}
-            **- Posts:** ${account.edge_owner_to_timeline_media.count}
-            **- Followers:** ${account.edge_followed_by.count}
-            **- Following:** ${account.edge_follow.count}
-            **- Private account:** ${account.is_private ? "Yes 🔐" : "Nope 🔓"}`);
-
-        message.channel.send(embed);
+        
     }
 }
